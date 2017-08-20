089c6490861d0e1d188c8d707d52575757a439b9
jscsc <jcstone3@ncsu.edu>
2017-08-20 10:15:34 -0400

    Added HelloWindow.cpp

--- LearnOpenGL/HelloWindow.cpp
+++ LearnOpenGL/HelloWindow.cpp
@@ -6,7 +6,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow *window);
 
-// settings
+// Constants
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
